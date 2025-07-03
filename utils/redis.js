5f732bfd445f6d8833ac4c6581de6bdbe4977a28
“Tristian <tristian.davis@atlasschool.com>
2025-07-03 14:22:11 -0500

    all of mine are giving the wrong response

--- utils/redis.js
+++ utils/redis.js
@@ -9,16 +9,45 @@ class RedisClient {
     this.connection = false;
     this.rClient.on('error', (err) => {
       console.log(err);
+      this.connection = false;
     });
     this.rClient.on('connect', () => {
       this.connection = true;
-    })
-    }
+    });
+  }
 
-    isAlive () {
-      return this.connection
+  isAlive () {
+    return this.connection;
+  }
+  
+  async get(key) {
+    const data = this.rClient.get(key);
+    try {
+      return await data
+  } catch (err) {
+      console.log('GET error', err);
+      return null;
+  }
+  }
+
+  async set(key, value, duration) {
+    const data = this.rClient.set(key, value, duration);
+    try {
+      return await data[3]
+    } catch (err) {
+      console.log('SET error', err)
     }
   }
+
+  async del(key) {
+    try {
+      this.rClient.del(key);
+    } catch (err) {
+      console.log('DEL error', err)
+    }
+  }
+
+}
 
 // create the new object redisClient
 const redisClient = new RedisClient();
