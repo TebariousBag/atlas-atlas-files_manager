d9d9d929aee96f67799c52256d6c74b3692687ee
“Tristian <tristian.davis@atlasschool.com>
2025-07-21 14:45:48 -0500

    added enpoints to index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import AppController from '../controllers/AppController.js';
 import UsersController from '../controllers/UsersController.js';
+import AuthController from '../controllers/AuthController.js';
 
 const newRouter = express.Router();
 // our endpoints for get status and stats
@@ -8,5 +9,8 @@ newRouter.get('/status', AppController.getStatus);
 newRouter.get('/stats', AppController.getStats);
 //endpoint for post
 newRouter.post('/users', UsersController.postNew);
+// endpoints or authconroller
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
 // and then export
 export default newRouter;
